34b6ba665e348a8df48b181dc197940ef147e50f
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-12-09 12:20:00 +0300

    add nivo pie chart

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: "rust",
+    label: "rust",
+    value: 168,
+    color: "hsl(310, 70%, 50%)"
+  },
+  {
+    id: "javascript",
+    label: "javascript",
+    value: 392,
+    color: "hsl(196, 70%, 50%)"
+  },
+  {
+    id: "c",
+    label: "c",
+    value: 21,
+    color: "hsl(314, 70%, 50%)"
+  },
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 76,
+    color: "hsl(235, 70%, 50%)"
+  },
+  {
+    id: "python",
+    label: "python",
+    value: 314,
+    color: "hsl(127, 70%, 50%)"
+  }
+];
